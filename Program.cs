23c632ccf3c1fa1ef63d7240a9dc1d32374efd00
KauanBarros <k.barros@aluno.senai.br>
2026-02-11 19:25:44 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//Nivel 1: variaveis e operações Basicas
+#region 1. calculadora de Media
+
+double primeira_nota = Console.Read();
+Console.Write("qual a segunda nota?");
+double segunda_nota = Console.Read();
+Console.Write("qual a terceira nota?");
+double terceira_nota = Console.Read();
+
+#endregion
